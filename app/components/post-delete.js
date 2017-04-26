c81ccd45861773ae5bd81dbe0f7ae45c1294769a
Chris Carr and Megan Warnock <chris@>
2017-04-26 14:34:42 -0700

    add update and delete functionality and custom button stylings

---
+++ app/components/post-delete.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    delete(post) {
+      debugger;
+      if(confirm('Are you sure you want to delete this post?')) {
+        this.sendAction('destroyPost', post);
+      }
+    }
+  }
+});
---
+++ app/components/post-update.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+    update(post) {
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        snippet: this.get('snippet'),
+        content: this.get('content'),
+        image: this.get('image'),
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
+});
