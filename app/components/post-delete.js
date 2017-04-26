e15c1c646cb89e60641bafb205907bac15e4e152
Chris Carr and Megan Warnock <chris@>
2017-04-26 14:55:15 -0700

    add custom styles

--- app/components/post-delete.js
+++ app/components/post-delete.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     delete(post) {
-      debugger;
       if(confirm('Are you sure you want to delete this post?')) {
         this.sendAction('destroyPost', post);
       }
