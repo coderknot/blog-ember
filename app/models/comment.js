26ab0f978307facdb0caad22d6b69faeb24ba741
Chris Carr and Megan Warnock <chris@>
2017-04-27 10:04:26 -0700

    add comments displaying on index

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,10 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr(),
-  author: DS.attr(),
-  snippet: DS.attr(),
+  name: DS.attr(),
   content: DS.attr(),
-  image: DS.attr(),
-  
 });
